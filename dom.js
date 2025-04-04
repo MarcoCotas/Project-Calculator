4398a973ddff6299e7c5713bc0afccb71d6a9dfb
MarcoCotas <marco.cotas06@gmail.com>
2025-04-04 20:34:01 +0100

    Create basic calculator logic

--- dom.js
+++ dom.js
@@ -1,4 +1,4 @@
-const buttons = document.querySelectorAll(".btn");
+const btn = document.querySelectorAll(".btn");
 const display = document.querySelector("#displayer");
 const equal = document.querySelector("#equal");
 const plus = document.querySelector("#add");
@@ -11,23 +11,46 @@ const negative = document.querySelector("#negative");
 let number1 = "";
 let number2 = "";
 let operator = undefined;
+display.textContent = "";
+let reset = false;
+
+btn.forEach((button) => {
+  button.addEventListener("click", () => {
+    const buttonText = button.textContent;
+    if (["Clear"].includes(buttonText)) {
+      display.textContent = 0;
+    }
+
+    if (["+", "-", "*", "/"].includes(buttonText)) {
+      number1 = parseFloat(display.textContent);
+      operator = buttonText;
+      display.textContent = ""; // Limpar para o próximo número
+    } else if (buttonText === "=") {
+      number2 = parseFloat(display.textContent);
+      operate(number1, operator, number2);
+      number1 = parseFloat(display.textContent);
+    } else {
+      display.textContent += buttonText;
+    }
+  });
+});
 
 function operate(number1, operator, number2) {
   if (operator === "+") {
-    return adiction(number1, number2);
+    display.textContent = addiction(number1, number2);
   } else if (operator === "-") {
-    return subtracion(number1, number2);
+    display.textContent = subtraction(number1, number2);
   } else if (operator === "*") {
-    return multiplication(number1, number2);
+    display.textContent = multiplication(number1, number2);
   } else if (operator === "/") {
-    return division(number1, number2);
+    display.textContent = division(number1, number2);
   }
 }
 
-function adiction(number1, number2) {
+function addiction(number1, number2) {
   return number1 + number2;
 }
-function subtracion(number1, number2) {
+function subtraction(number1, number2) {
   return number1 - number2;
 }
 

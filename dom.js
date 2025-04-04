4183c7f9919853c806a05346faa1e97a7bbcbfb2
MarcoCotas <marco.cotas06@gmail.com>
2025-04-04 19:11:19 +0100

    Assign all variabels

--- dom.js
+++ dom.js
@@ -1,27 +1,39 @@
 const buttons = document.querySelectorAll(".btn");
-const display = document.querySelector("displayer");
+const display = document.querySelector("#displayer");
+const equal = document.querySelector("#equal");
+const plus = document.querySelector("#add");
+const minus = document.querySelector("#minus");
+const divide = document.querySelector("#divide");
+const times = document.querySelector("#times");
+const float = document.querySelector("#float");
+const clear = document.querySelector("#clear");
+const negative = document.querySelector("#negative");
 let number1 = "";
 let number2 = "";
 let operator = undefined;
-let displayer = 0;
 
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    console.log(innerHTML);
-  });
-});
-
-function operate(number1, operator, number2){
-  if (operator === "+"){
-    return add(number1, number2)
-  } else if (operator === "-"){
-    return subtract(number1, number2)
+function operate(number1, operator, number2) {
+  if (operator === "+") {
+    return adiction(number1, number2);
+  } else if (operator === "-") {
+    return subtracion(number1, number2);
+  } else if (operator === "*") {
+    return multiplication(number1, number2);
+  } else if (operator === "/") {
+    return division(number1, number2);
   }
 }
 
-function add(number1, number2){
-  return (number1 + number2)
+function adiction(number1, number2) {
+  return number1 + number2;
 }
-function subtract(number1, number2){
-  return (number1  number2)
+function subtracion(number1, number2) {
+  return number1 - number2;
+}
+
+function multiplication(number1, number2) {
+  return number1 * number2;
+}
+function division(number1, number2) {
+  return number1 / number2;
 }
